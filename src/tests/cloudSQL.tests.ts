03243a32cb34b46f2409865c8aad48e49326009c
Vitali Sonchyk <vitali.sonchyk@ensek.co.uk>
2026-09-07 12:23:58 +0200

    fix: Correct expected usage limit in Cloud SQL tests

--- src/tests/cloudSQL.tests.ts
+++ src/tests/cloudSQL.tests.ts
@@ -21,7 +21,7 @@ test.describe('Cloud SQL', () => {
       { tag: Tags.Smoke },
       async ({ cloudSQLSteps }) => {
         const actualUsageLimit = await cloudSQLSteps.getTotalUsageLimit(usageLimit.instances);
-        expect(actualUsageLimit).toEqual(26);
+        expect(actualUsageLimit).toEqual(usageLimit.expectedUsageLimit);
       },
     );
   });
